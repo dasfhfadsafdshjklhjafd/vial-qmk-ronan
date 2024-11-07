4fb2d71f41b84299b68b5438a97de85946c6cff5
dasfhfadsafdshjklhjafd <74335146+dasfhfadsafdshjklhjafd@users.noreply.github.com>
2024-11-07 21:01:08 +0100

    added config for sunaku hrm

--- keyboards/astronaut/keymaps/default/config.h
+++ keyboards/astronaut/keymaps/default/config.h
@@ -1,12 +1,3 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x04, 0x7F, 0x41, 0xF3, 0x39, 0xD8, 0x1F, 0xEA}
-// #define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
-// #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
-
-
 #define TAPPING_TERM 200
 //#define IGNORE_MOD_TAP_INTERRUPT /* for rolling on mod-tap keys */
 
