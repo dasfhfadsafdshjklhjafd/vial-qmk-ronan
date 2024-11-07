f5fe502837ef1b86c98d76d26fa173be13d58777
dasfhfadsafdshjklhjafd <74335146+dasfhfadsafdshjklhjafd@users.noreply.github.com>
2024-11-07 20:42:31 +0100

    fixed vial json

--- keyboards/astronaut/keymaps/vial/keymap.c
+++ keyboards/astronaut/keymaps/vial/keymap.c
@@ -63,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        CTL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_QUOT,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_CAPS,         FKEYS, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                        ADJUST,  KC_LGUI, ALT_ENT, KC_SPC,   NAV,               SYM,   KC_SPC, KC_RALT, KC_RGUI, KC_APP
+        KC_ESCAPE,   KC_B,    KC_L,    KC_D,    KC_W,    KC_Z,                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,  LGUI_T(KC_N),    LALT_T(KC_R),    LCTL_T(KC_T),    LSFT_T(KC_S),    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_QUOT,
+        KC_LSFT,  KC_Q,    KC_X,    KC_M,    KC_C,    KC_V,    MO(3), KC_CAPS,         FKEYS, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                        TD(4),  KC_LSFT, KC_LCTL, KC_BSPC,   KC_NO,               SYM,   KC_SPC, KC_RALT, KC_RGUI, KC_APP
     ),
 
 
